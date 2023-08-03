209676df9699566cd511a347ed519a637c6dcfc8
gaeguriBanchan <rlawowls93@gamil.com>
2023-08-03 16:21:59 +0900

    프로젝트 생성, ThemeData 설정

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 2,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
